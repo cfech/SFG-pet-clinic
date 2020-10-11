a0704e38dcd65dc4b170d93a1377d4cb4faf9cb1
Connor <cfech44@gmail.com>
2020-10-10 23:11:05 -0400

    added owners list to thyemleaf template  and updated controller. closes #16

--- pet-clinic-web/src/main/java/com/example/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/example/sfgpetclinic/controllers/OwnerController.java
@@ -1,7 +1,9 @@
 package com.example.sfgpetclinic.controllers;
 
 
+import com.example.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 //root path
@@ -9,11 +11,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnerController {
+    private final OwnerService ownerService;
+
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
 
     //these paths extend the base path, ie /owners/index will also return the
     // ownersList html file
     @RequestMapping({"","/", "/index", "/index.html"})
-    public String ownerIndex(){
+
+    //spring mvc will automatically add a model when run
+    public String ownerIndex(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
 
         return "owners/ownersList";
     }
